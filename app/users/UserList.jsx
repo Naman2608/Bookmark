f901aabeffad287b62de1c8957c57b19886cde23
Naman Chhabra <72180587+Naman2608@users.noreply.github.com>
2024-03-10 20:58:17 +0530

    Update UserList.jsx

--- app/users/UserList.jsx
+++ app/users/UserList.jsx
@@ -1,11 +1,7 @@
 import getData from "@/firebase/firestore/getData";
 
 async function getUsers() {
-    // const res = await fetch('http://localhost:4000/users', {
-    //     next: {
-    //         revalidate: 0
-    //     }
-    // })
+  
     const { res, error } = await getData('users')
 
     if (error) {
@@ -20,26 +16,7 @@ export default async function UserList() {
 
     return (
         <>
-            {/* {
-                users.map((user) => (
-                    <div key={user.id} className="card my-5">
-                        <table>
-                            <tr>
-                                <td><b>Name</b>:{user.Name}</td>
-                                <td><b>Email</b>{user.Email}</td>
-                                <td><b>City</b>{user.City}</td>
-                                <td><b>Mobile Number</b>{user.Number}</td>
-
-                            </tr>
-                        </table>
-                    </div>
-                ))
-            }
-            {users.length === 0 && (
-                <p className="text-center">
-                    No User Found in Database
-                </p>
-            )} */}
+            {}
         </>
 
     )
